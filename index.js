bb076f15a1ebe41a7f1289f46943ec4b2eb21526
cibernox <miguel.camba@gmail.com>
2016-01-15 09:44:03 +0000

    Move dependencies around

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-power-select-sortable'
+  name: 'ember-power-select-sortable',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+  },
+
+  contentFor: function(type, config) {
+    var emberPowerSelect = this.addons.filter(function(addon) {
+      return addon.name === 'ember-power-select';
+    })[0]
+    return emberPowerSelect.contentFor(type, config);
+  }
 };
