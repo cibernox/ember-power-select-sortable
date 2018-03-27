69571d64313fe6b0fe9ac235c7bb03a3c6147fa5
Ilya Radchenko <ilya@burstcreations.com>
2018-03-27 13:17:05 -0400

    fix: remove unmaintained pagefront plugin, fix lint and lock files

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
   options: ['one', 'two', 'three', 'four', 'seven']
 });
